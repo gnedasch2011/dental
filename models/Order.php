4d0c0df8ba0e9de3ed0ca848227950e5c0baa5c8
Maks <gnedasch2011@yandex.ru>
2019-10-17 17:30:01 +0300

    footer

---
+++ models/Order.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "order".
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $phone
+ * @property string $created_at
+ * @property string $text
+ */
+class Order extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'order';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id'], 'required'],
+            [['id'], 'integer'],
+            [['name', 'phone', 'created_at', 'text'], 'string', 'max' => 45],
+            [['id'], 'unique'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'phone' => 'Phone',
+            'created_at' => 'Created At',
+            'text' => 'Text',
+        ];
+    }
+}
